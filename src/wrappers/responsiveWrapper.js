452e3bdc412353ed238926aa86cabaf47791ab8f
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-29 18:19:27 -0400

    wrappers: moved responsiveWrapper to own dir

--- src/wrappers/responsiveWrapper.js
+++ src/wrappers/responsiveWrapper.js
@@ -5,19 +5,20 @@ import { Responsive } from "semantic-ui-react";
  * HOF used for rendering a single Component responsively
  * - provides a "mobile" boolean prop for widths up to SUIR Responsive.onlyTablet.maxWidth
  * - does not provide "mobile" prop for widths above SUIR Responsive.onlyComputer.minWidth
+ * - curries any additional props provided
  * @param {func} Component to render with or without "mobile" prop
  */
 const responsiveWrapper = Component => props => (
-	<>
-		<Responsive
-			children={<Component {...props} mobile />}
-			maxWidth={Responsive.onlyTablet.maxWidth}
-		/>
-		<Responsive
-			children={<Component {...props} />}
-			minWidth={Responsive.onlyComputer.minWidth}
-		/>
-	</>
+  <>
+    <Responsive
+      children={<Component {...props} mobile />}
+      maxWidth={Responsive.onlyTablet.maxWidth}
+    />
+    <Responsive
+      children={<Component {...props} />}
+      minWidth={Responsive.onlyComputer.minWidth}
+    />
+  </>
 );
 
 export default responsiveWrapper;
