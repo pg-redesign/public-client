20c4f3989e252a992ff6aa1b8dc68d6673be4bf9
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-29 18:27:15 -0400

    CourseCard: refactor to use API data

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -4,84 +4,82 @@ import { Card, Icon, List, Popup, Button, Header } from "semantic-ui-react";
 
 import { courseType } from "../utils/prop-types";
 
-const courseShortDescription = descriptionData =>
-	descriptionData.map(description => (
-		<List.Item>
-			<List.Content content={description} />
-		</List.Item>
-	));
-
 export const CourseCardTop = props => {
-	const { name, location, date } = props;
+  const { name, location, date } = props;
 
-	return (
-		<Card.Content textAlign="left">
-			<Card.Header textAlign="center" content={name} />
-			<Card.Meta textAlign="center">
-				{date}
-				{" - "}
-				<Popup
-					size="tiny"
-					position="bottom center"
-					content="click to view hotel on Google Maps"
-					trigger={
-						<a href={location.url} target="_blank" rel="noopener noreferrer">
-							{location.city}, {location.state}, {location.country}&nbsp;
-							<Icon name="map marker alternate" color="red" />
-						</a>
-					}
-				/>
-			</Card.Meta>
-		</Card.Content>
-	);
+  return (
+    <Card.Content textAlign="left">
+      <Card.Header textAlign="center" content={name} />
+      <Card.Meta textAlign="center">
+        {date}
+        {" - "}
+        <Popup
+          size="tiny"
+          position="bottom center"
+          content="click to view hotel on Google Maps"
+          trigger={
+            <a href={location.mapURL} target="_blank" rel="noopener noreferrer">
+              {location.concatenated}&nbsp;
+              <Icon name="map marker alternate" color="red" />
+            </a>
+          }
+        />
+      </Card.Meta>
+    </Card.Content>
+  );
 };
 
 CourseCardTop.propTypes = {
-	name: courseType.name,
-	date: courseType.date,
-	location: courseType.location,
+  name: courseType.name,
+  date: courseType.date,
+  location: courseType.location,
 };
 
+const makeDescriptionList = (courseId, descriptions) =>
+  descriptions.map((description, index) => (
+    <List.Item key={`course-${courseId}-description-${index}`}>
+      <List.Content content={description} />
+    </List.Item>
+  ));
+
 const CourseCard = props => {
-	const { id, shortName, description } = props;
+  const { id, shortName, description } = props;
 
-	return (
-		<Card raised color="grey">
-			{/* header - name, date, location */}
-			<CourseCardTop {...props} />
+  return (
+    <Card raised color="grey">
+      {/* header - name, date, location */}
+      <CourseCardTop {...props} />
 
-			{/* content - short description */}
-			<Card.Content extra textAlign="left">
-				<Card.Description>
-					<Header size="small" content="Course Topic Highlights" />
-					{/* TODO: pull from API, short and long description arrays */}
-					<List bulleted content={courseShortDescription(description.short)} />
-				</Card.Description>
-			</Card.Content>
+      {/* content - short description */}
+      <Card.Content extra textAlign="left">
+        <Card.Description>
+          <Header size="small" content="Course Topic Highlights" />
+          {/* TODO: pull from API, short and long description arrays */}
+          <List bulleted content={makeDescriptionList(id, description)} />
+        </Card.Description>
+      </Card.Content>
 
-			{/* content - register / more info buttons */}
-			<Card.Content extra>
-				<Button.Group fluid>
-					<Button
-						rounded
-						as={Link}
-						size="large"
-						content="Register Now"
-						to={`/register?courseID=${id}`}
-						style={{ backgroundColor: "var(--dark-blue)", color: "white" }}
-					/>
-					<Button.Or />
-					<Button
-						as={Link}
-						rounded
-						size="large"
-						content="Learn More"
-						to={`/courses/${shortName}`}
-					/>
-				</Button.Group>
-			</Card.Content>
-		</Card>
-	);
+      {/* content - register / more info buttons */}
+      <Card.Content extra>
+        <Button.Group fluid>
+          <Button
+            as={Link}
+            size="large"
+            content="Register Now"
+            to={`/register/${id}`}
+            style={{ backgroundColor: "var(--dark-blue)", color: "white" }}
+          />
+          <Button.Or />
+          <Button
+            as={Link}
+            size="large"
+            content="Learn More"
+            to={`/courses/${shortName}`}
+          />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
 };
 
 CourseCard.propTypes = courseType;
