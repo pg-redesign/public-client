21a37c4e66820d6326942a0dc0b68eb5188bb0fe
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-28 23:40:20 -0400

    StaticAnimation: wraps a component with controllable animation

---
+++ src/components/StaticAnimation.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Transition } from "semantic-ui-react";
+
+class StaticAnimation extends Component {
+  static propTypes = {
+    duration: PropTypes.number,
+    animate: PropTypes.bool.isRequired,
+    children: PropTypes.element.isRequired,
+    animation: PropTypes.oneOf([
+      "jiggle",
+      "flash",
+      "shake",
+      "pulse",
+      "tada",
+      "bounce",
+      "glow",
+    ]).isRequired,
+  };
+
+  static defaultProps = {
+    duration: 500,
+    animation: "shake",
+  };
+
+  state = {
+    visible: true,
+  };
+
+  componentDidUpdate = previousProps => {
+    const { animate } = this.props;
+
+    if (animate && !previousProps.animate) {
+      this.animateChildren();
+    }
+  };
+
+  animateChildren = () => this.setState(state => ({ visible: !state.visible }));
+
+  render = () => {
+    const { visible } = this.state;
+    const { animation, duration, children } = this.props;
+
+    return (
+      <Transition animation={animation} duration={duration} visible={visible}>
+        {children}
+      </Transition>
+    );
+  };
+}
+
+export default StaticAnimation;
