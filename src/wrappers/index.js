faff50324569ad60c24fc86b6601b28f00b86e91
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-30 23:18:13 -0400

    wrappers: withHandledQuery HOF QueryHandler alias

--- src/wrappers/index.js
+++ src/wrappers/index.js
@@ -1,2 +1,3 @@
+export { default as withHandledQuery } from "./withHandledQuery";
 export { default as responsiveWrapper } from "./responsiveWrapper";
 export { default as withUpcomingCourses } from "./withUpcomingCourses";
---
+++ src/wrappers/withHandledQuery.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import QueryHandler from "../components/QueryHandler";
+
+/**
+ * HOF used for exporting a Consumer component with a data requirement
+ * - HOF alias for <QueryHandler> wrapper component
+ * - provides a data prop to the Consumer component
+ * - curries any additional props
+ * @param {func} Consumer receives props.data and curried props
+ */
+const withHandledQuery = (Consumer, query, variables) => props => (
+  <QueryHandler
+    query={query}
+    Consumer={Consumer}
+    variables={variables}
+    {...props}
+  />
+);
+
+export default withHandledQuery;
