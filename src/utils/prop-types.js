0ebd8e559225eaf1c912f043e05aff619f74749d
Vamp <vampiirecodes@gmail.com>
2019-06-04 18:02:40 -0400

    shared prop types: refactor and add student shape

--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -1,24 +1,24 @@
 import PropTypes from "prop-types";
 
-// future if description is split into short and long
-// export const courseDescriptionType = PropTypes.shape({
-//   short: PropTypes.arrayOf(PropTypes.string),
-//   long: PropTypes.arrayOf(PropTypes.string),
-// });
-
-export const courseLocationType = PropTypes.shape({
-  mapURL: PropTypes.string.isRequired,
-  concatenated: PropTypes.string,
+// -- LOCATION -- //
+const locationType = {
   city: PropTypes.string,
   state: PropTypes.string,
   country: PropTypes.string,
+  concatenated: PropTypes.string,
+};
+
+// -- COURSE -- //
+export const courseLocationType = PropTypes.shape({
+  mapURL: PropTypes.string.isRequired,
+  ...locationType,
 });
 
 export const idType = PropTypes.oneOfType([PropTypes.string, PropTypes.number]);
 
-// TODO: course price / cost?
 export const courseType = {
   id: idType.isRequired,
+  price: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   shortName: PropTypes.string.isRequired,
@@ -27,3 +27,17 @@ export const courseType = {
 };
 
 export const courseTypeShape = PropTypes.shape(courseType);
+
+// -- STUDENT -- //
+export const studentType = {
+  id: idType.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  company: PropTypes.string,
+  location: PropTypes.shape(locationType),
+  paymentType: PropTypes.oneOf(["CHECK", "CREDIT"]),
+  hasPaid: PropTypes.bool,
+};
+
+export const studentTypeShape = PropTypes.shape(studentType);
