f68b6f733c5350526d601443c404ecd60b052e8f
Vamp <vampiirecodes@gmail.com>
2019-06-06 15:13:51 -0400

    StaticAnimation: moved to FormTools

---
+++ src/components/FormTools/index.js
@@ -0,0 +1,7 @@
+export * from "./LabelOrError";
+export * from "./MutationModal";
+export * from "./StaticAnimation";
+
+export { default as LabelOrError } from "./LabelOrError";
+export { default as MutationModal } from "./MutationModal";
+export { default as StaticAnimation } from "./StaticAnimation";
