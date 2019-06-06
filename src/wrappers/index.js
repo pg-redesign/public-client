1b3cfbc83e099c1d8311baa8023d500c00bcb1e7
Vamp <vampiirecodes@gmail.com>
2019-06-06 16:45:39 -0400

    wrappers: withStripeProvider, injects stripe prop

--- src/wrappers/index.js
+++ src/wrappers/index.js
@@ -1,3 +1,4 @@
 export { default as withHandledQuery } from "./withHandledQuery";
 export { default as responsiveWrapper } from "./responsiveWrapper";
+export { default as withStripeProvider } from "./withStripeProvider";
 export { default as withUpcomingCourses } from "./withUpcomingCourses";
