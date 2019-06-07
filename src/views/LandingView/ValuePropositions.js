2572e685e6ef6723914b1e06fcaec43763e19d17
Vamp <vampiirecodes@gmail.com>
2019-06-07 18:16:16 -0400

    ValueProps: reworded, reordered, new icons

--- src/views/LandingView/ValuePropositions.js
+++ src/views/LandingView/ValuePropositions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Icon, Card } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 
 const valuePropCardData = (() => {
   const iconProps = {
@@ -9,30 +9,28 @@ const valuePropCardData = (() => {
 
   return [
     {
-      // "trophy"
-      headerIcon: <Icon className="fas fa-trophy" {...iconProps} />,
-      description:
-        "Taught by award-winning, globally recognized groundwater experts",
+      headerIcon: <i className="fas fa-graduation-cap fa-2x" {...iconProps} />,
+      description: (
+        <>
+          Over <strong>20,000 graduates</strong> from leading companies and
+          government agencies
+        </>
+      ),
     },
     {
-      // "microscope" "user-graduate"
-      // does not work with <Icon> component, fa-2x == "big"
-      headerIcon: <i className="fas fa-microscope fa-2x" {...iconProps} />,
+      headerIcon: <i className="fas fa-globe-americas fa-2x" {...iconProps} />,
+      description:
+        "Taught by globally recognized experts in groundwater hydrology & remediation",
+    },
+    {
+      headerIcon: <i className="fas fa-clipboard-check fa-2x" {...iconProps} />,
+      description:
+        "Groundwater concepts, principles, technologies, and best practices",
+    },
+    {
+      headerIcon: <i className="fas fa-university fa-2x" {...iconProps} />,
       description:
         "Designed for  Geologists, Engineers, Chemists and Environmental Scientists",
-    },
-    {
-      // "hard-hat" "clipboard-check"
-      headerIcon: <Icon className="fas fa-clipboard-check" {...iconProps} />,
-      description:
-        "Academic & practical principles, technologies, and best practices",
-      // version 2 (takes up 3-4 lines, not consistent with other value props)
-      // "Comprehensive & practical knowledge of groundwater principles, technologies, and best practices",
-    },
-    {
-      // "university" "graduation-cap"
-      headerIcon: <Icon className="fas fa-graduation-cap" {...iconProps} />,
-      description: "Board approved for Continuing Education Credits (CEU)",
     },
   ];
 })();
