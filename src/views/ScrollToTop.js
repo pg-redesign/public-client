dc4c38dc8367cf3651074555f3281d2dcd36b854
Vamp <vampiirecodes@gmail.com>
2019-08-23 16:22:15 -0400

    ScrollToTop: Views wrapper that scrolls to top on nav change
    - add to Views
    - add CourseView Route

---
+++ src/views/ScrollToTop.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class ScrollToTop extends Component {
+  componentDidUpdate(previousProps) {
+    if (this.props.location.pathname === previousProps.location.pathname) {
+      // only scroll if moving to different view
+      return;
+    }
+
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+export default withRouter(ScrollToTop);
