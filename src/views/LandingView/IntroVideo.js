5e94bd5b9bbc51b2d56dbfc3c795bb9eca42e80e
Vamp <vampiirecodes@gmail.com>
2019-10-15 12:19:28 -0500

    IntroVideo: fix suggested video at end of intro

--- src/views/LandingView/IntroVideo.js
+++ src/views/LandingView/IntroVideo.js
@@ -5,7 +5,6 @@ import { Embed } from "semantic-ui-react";
 const IntroVideo = props => (
   <Embed
     defaultActive
-    source="youtube"
     iframe={{ allowFullScreen: true }}
     url={`https://www.youtube.com/embed/${props.videoId}?autohide=true&amp;autoplay=false&amp;color=%23444444&amp;hq=true&amp;jsapi=false&amp;modestbranding=false&amp;rel=0`}
   />
