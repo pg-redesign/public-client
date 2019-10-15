5c3dbcc873bb92ace874657fc6dd52d3ad823b7c
Vamp <vampiirecodes@gmail.com>
2019-10-15 12:19:46 -0500

    CompanyAndAgencyLogos: add all new logos

--- src/views/LandingView/CompanyAndAgencyLogos.js
+++ src/views/LandingView/CompanyAndAgencyLogos.js
@@ -2,31 +2,51 @@ import React from "react";
 import { Image, ImageGroup } from "semantic-ui-react";
 import { responsiveWrapper } from "../../wrappers";
 
-const logoSources = {
-  DOW: require("../../media/logos/dow.jpeg"),
-  usEPA: require("../../media/logos/usEPA.png"),
-  Shell: require("../../media/logos/shell.png"),
-  brCEB: require("../../media/logos/brCEB.png"),
-  Monsanto: require("../../media/logos/monsanto.jpg"),
-};
+const logos = [
+  "dow.jpeg",
+  "usEPA.png",
+  "shell.png",
+  "brCEB.png",
+  "monsanto.jpg",
+  "AMEC Foster Wheeler.png",
+  "GZA.jpg",
+  "US Army Corps of Engineers.png",
+  "US Navy.jpg",
+  "California Water Boards.jpg",
+  "ExxonMobile.png",
+  "Nuclear Reg Comm (NRC).png",
+  "BARR Eng..png",
+  "Vale-Brasil.png",
+  "TX Comm Envir Qual.png",
+  "Brasil Transpetro.png",
+  "Bristol-Myers Squibb.png",
+  "AECOM.png",
+  "USGS.png",
+  "United Technologies.jpg",
+  "Golder-Logo.jpg",
+  "Landau & Assoc.jpg",
+  "National Energy Board.jpg",
+  "Regenesis.jpg",
+  "BNSF Railway.png",
+  "ConocoPhillips.png",
+  "Esso=Imperial Oil.png",
+  "GeoKlock-Brasil.jpg",
+  "RAMBOLL.jpg",
+  "SNC-LAVALIN.png",
+];
 
 const CompanyAndAgencyLogos = props => {
   const { mobile } = props;
 
-  const { usEPA, Shell, brCEB, DOW, Monsanto } = logoSources;
-  const mobileLogoOrder = [usEPA, Shell, brCEB, DOW, Monsanto];
-  const standardLogoOrder = [usEPA, DOW, Shell, Monsanto, brCEB];
-
   return (
     <ImageGroup size={mobile ? "tiny" : "small"}>
-      {(mobile ? mobileLogoOrder : standardLogoOrder).map((source, index) => (
+      {logos.map((source, index) => (
         <Image
           rounded
-          src={source}
+          src={require(`../../media/company-logos/${source}`)}
           key={`attendee-logos-${index}`}
           style={{
             filter: "opacity(1) drop-shadow(2px 3px 1px rgba(0,0,0,0.4))",
-            margin: mobile ? "10px 10px" : "0 10px",
           }}
         />
       ))}
